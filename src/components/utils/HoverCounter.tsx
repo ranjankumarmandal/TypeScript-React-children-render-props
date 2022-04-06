cc24f7900fbb3711ef050768131d05adcd5b4e6d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-07 00:45:49 +0530

    completed component HoverCounter.tsx

--- src/components/utils/HoverCounter.tsx
+++ src/components/utils/HoverCounter.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Props } from './ClickCounter';
 
 const HoverCounter = (props: Props) => {
-  return <div>HoverCounter</div>;
+  return (
+    <div>
+      <h2 onMouseOver={props.increamentCounter}>
+        Mouse Over Increamentor: {props.count}
+      </h2>
+    </div>
+  );
 };
 
 export default HoverCounter;
