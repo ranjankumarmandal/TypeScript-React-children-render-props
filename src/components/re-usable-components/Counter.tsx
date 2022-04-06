62dda7f561c506dc7a4d38a82fff5c7db109211e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-07 00:20:47 +0530

    created a state count with type inference number in re-usable component Counter.tsx

--- src/components/re-usable-components/Counter.tsx
+++ src/components/re-usable-components/Counter.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   render: () => JSX.Element | null;
 }
 
 const Counter = (props: Props) => {
+  const [count, setCount] = useState(0);
+
   return <div>Counter</div>;
 };
 
