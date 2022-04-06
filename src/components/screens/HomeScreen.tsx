6ce5d54a5771eb71559714a2fdfe5d6cc29aa9a9
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-07 00:39:59 +0530

    added implementation of render props in HomeScreen.tsx with re-usable Component Counter.tsx for ClickCounter.tsx

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Counter from '../re-usable-components/Counter';
+import ClickCounter from '../utils/ClickCounter';
 
 const HomeScreen = () => {
   return (
     <div>
       <h1>Welcome, Let's Research!</h1>
+      <Counter
+        render={(count, increamentCount) => (
+          <ClickCounter count={count} increamentCounter={increamentCount} />
+        )}
+      />
     </div>
   );
 };
