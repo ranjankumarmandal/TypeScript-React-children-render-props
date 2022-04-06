e694f9a3428409978b7a523fffa6fa7ee702f126
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-07 00:04:29 +0530

    completed component AboutScreen.tsx

--- src/components/screens/AboutScreen.tsx
+++ src/components/screens/AboutScreen.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const AboutScreen = () => {
-  return <div>AboutScreen</div>;
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>App Name - React Concepts with TypeScript</p>
+      <p>Description - TypeScript + React + React Router Dom v5.0.0 </p>
+      <p>
+        By -{' '}
+        <b>
+          <a
+            style={{ textDecoration: 'none' }}
+            href='https://www.linkedin.com/in/ranjan-kumar-m-818367158/'
+            target='_blank'
+          >
+            Ranjan Kumar Mandal
+          </a>
+        </b>
+      </p>
+    </div>
+  );
 };
 
 export default AboutScreen;
