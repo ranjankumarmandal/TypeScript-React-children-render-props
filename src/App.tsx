d354b48b358e7961e608d9fbb496d0176b88ba16
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 23:21:31 +0530

    created components HomeScreen.tsx and AboutScreen.tsx and added them to routes in root component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomeScreen from './components/screens/HomeScreen';
+import AboutScreen from './components/screens/AboutScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' component={HomeScreen} />
-        <Route path='/about' component={About} />
+        <Route path='/about' component={AboutScreen} />
       </Switch>
     </BrowserRouter>
   );
