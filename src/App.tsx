17ac9242a1ca97982b706d490b3d0e586ebb591e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-06 23:37:29 +0530

    added HomeContainer.tsx component to App.tsx - we can see props.children is effective now

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomeScreen from './components/screens/HomeScreen';
 import AboutScreen from './components/screens/AboutScreen';
+import HomeContainer from './components/containers/HomeContainer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={HomeScreen} />
-        <Route exact path='/about' component={AboutScreen} />
-      </Switch>
+      <HomeContainer>
+        <Switch>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/about' component={AboutScreen} />
+        </Switch>
+      </HomeContainer>
     </BrowserRouter>
   );
 }
